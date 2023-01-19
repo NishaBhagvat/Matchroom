07c2b20f30d255d57826224557a6296cd4acbcc1
Nisha <nisha.bhagvat@zego.com>
2023-01-19 22:52:13 +0000

    change browser

--- src/test/java/com/matchroom/runners/MyRunnerTestSuite.java
+++ src/test/java/com/matchroom/runners/MyRunnerTestSuite.java
@@ -2,9 +2,8 @@ package com.matchroom.runners;
 
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
-import net.thucydides.core.annotations.Managed;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
+
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
@@ -14,6 +13,4 @@ import org.openqa.selenium.WebDriver;
 )
 
 public class MyRunnerTestSuite {
-    @Managed(driver = "firefox")
-    WebDriver driver;
 }
